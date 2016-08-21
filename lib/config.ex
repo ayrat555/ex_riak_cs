b1f2909bc1e30d4a1e187a00f5eef426add22785
Ayrat Badykov <ayratin555@gmail.com>
2016-08-21 13:07:27 +0300

    add docs to object api and config

--- lib/config.ex
+++ lib/config.ex
@@ -1,5 +1,23 @@
 defmodule ExRiakCS.Config do
 
+  @moduledoc """
+    A set of configuration parameters for Riak CS API
+
+    You can configure ex_riak_cs by setting configuration parameters in your Mix.Config files
+
+        config :ex_riak_cs,
+          key_id: "test_id", # your access key
+          secret_key: "test_key", # your secret key
+          schema: "https://", # your riak cs server schema
+          host: "storage-nginx.stage.govermedia.com" # your riak cs server host
+
+
+    Additional parameters
+
+          exp_days: 2 # number of days a signed upload part url will be valid. default value - 1
+          acl: "private" # created file's acl. default value - "public-read"
+  """
+
   def secret_key do
     env_var!(:secret_key)
   end
@@ -24,7 +42,7 @@ defmodule ExRiakCS.Config do
     Application.fetch_env!(:ex_riak_cs, key)
   end
 
-  def env_var(key, default \\ nil) do
+  defp env_var(key, default \\ nil) do
     Application.get_env(:ex_riak_cs, key, default)
   end
 end
--- lib/object.ex
+++ lib/object.ex
@@ -1,6 +1,21 @@
 defmodule ExRiakCS.Object do
   alias ExRiakCS.Request
 
+  @moduledoc """
+  This module contains object-level operations
+
+  More info at: http://docs.basho.com/riak/cs/2.1.1/references/apis/storage/#object-level-operations
+  """
+
+  @doc """
+  Deletes an object and returns {:ok, nil} if request was successful, otherwise returns {:error, {response_code, response_body} }
+
+  ## Example
+
+      {:ok, _} = Object.delete("test-bucket", "key")
+
+  More info at: http://docs.basho.com/riak/cs/2.1.1/references/apis/storage/s3/delete-object/
+  """
   def delete(bucket, key) do
     path = "/#{bucket}/#{key}"
     case Request.request(:delete, path) do
@@ -8,6 +23,16 @@ defmodule ExRiakCS.Object do
       %{status_code: code, body: body} -> {:error, {code, body}}
     end
   end
+
+  @doc """
+  Retrieves object metadata (not the full content of the object) and returns {:ok, headers} if request was successful, otherwise returns {:error, {response_code, response_body} }
+
+  ## Example
+
+      {:ok, headers} = Object.head("test-bucket", "key")
+
+  More info at: http://docs.basho.com/riak/cs/2.1.1/references/apis/storage/s3/head-object/
+  """
 
   def head(bucket, key) do
     path = "/#{bucket}/#{key}"
